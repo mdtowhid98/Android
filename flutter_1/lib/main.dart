67ce9b3e52cafb77e5b9987d47b4fefa5c7271bf
Md Towhidul Alam <158472221+mdtowhid98@users.noreply.github.com>
2024-10-17 12:56:30 +0600

    add

--- flutter_1/lib/main.dart
+++ flutter_1/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,119 +8,118 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.green, // Light theme
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.amber, // Dark theme
+      ),
+      themeMode: ThemeMode.system,
+      // Automatically switch between light and dark
+      debugShowCheckedModeBanner: false,
+      home: HomeActivity(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  MySnackBar(message, context) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(message))
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Inventory App"),
+        titleSpacing: 10,
+        toolbarHeight: 60,
+        toolbarOpacity: 1,
+        elevation: 4,
+        backgroundColor: Colors.green,
+        actions: [
+
+          IconButton(onPressed: () {
+            MySnackBar("I am Comments", context);
+          }, icon: Icon(Icons.comment)),
+          IconButton(onPressed: () {
+            MySnackBar("I am Search", context);
+          }, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {
+            MySnackBar("I am Settings", context);
+          }, icon: Icon(Icons.settings)),
+          IconButton(onPressed: () {
+            MySnackBar("I am Email", context);
+          }, icon: Icon(Icons.email)),
+
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+
+      floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.green,
+        onPressed: () {
+          MySnackBar("I am floating action button", context);
+        },
+
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: "Contact"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+
+        onTap: (int index) {
+          if (index == 0) {
+            MySnackBar("I am Home bottom menu", context);
+          }
+          if (index == 1) {
+            MySnackBar("I am Contact bottom menu", context);
+          }
+
+          if (index == 2) {
+            MySnackBar("I am Profile bottom menu", context);
+          }
+        },
+
+      ),
+
+      drawer: Drawer(
+
+        child: ListView(
+          children: [
+            DrawerHeader(child: Text("Towhid")),
+            ListTile(leading: Icon(Icons.home),
+              title: Text("Home"),
+              onTap: (){MySnackBar("I am Home", context);},),
+            ListTile(leading: Icon(Icons.contact_mail),
+                title: Text("Contact"),
+              onTap: (){MySnackBar("I am Contact", context);},),
+            ListTile(leading: Icon(Icons.person),
+                title: Text("Profile"),
+              onTap: (){MySnackBar("I am Profile", context);},),
+            ListTile(leading: Icon(Icons.email),
+                title: Text("Email"),
+              onTap: (){MySnackBar("I am Email", context);},),
+            ListTile(leading: Icon(Icons.phone),
+                title: Text("Phone"),
+              onTap: (){MySnackBar("I am Phone", context);},),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
     );
   }
 }
